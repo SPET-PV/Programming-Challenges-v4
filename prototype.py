05a1eba86edb5f9e9f6c42298520df568d0be773
Spet-PV <SPET-PV@protonmail.com>
2023-06-04 01:21:59 +0100

    Challenge 46 beta.v1.0

--- prototype.py
+++ prototype.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import string
+# This is the beginning of the program
+#Init
+RawText = input("Enter your text :\n")
+Char_Choice = input("What do you want to find :\n")
+Total = 0
+# Treatment
+for i in range(0,len(RawText)):
+    if RawText[i] == Char_Choice:
+        Total += 1
+# Output
+print(f"The Number of Occurences of this Character is :\"{Total}\"")
