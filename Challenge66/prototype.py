38fb2736dcc19d9dad68419913b60a54bf8b4a5e
Spet-PV <SPET-PV@protonmail.com>
2023-06-12 01:40:54 +0100

    Text to Hex/Bin/Oct Betav0.2

--- Challenge66/prototype.py
+++ Challenge66/prototype.py
@@ -4,12 +4,16 @@ import string
 RawText = input("Enter your text : \n")
 HexOutput = []
 BinOutput = []
+OctOutput = []
 #Treatment
 for i in range(0,len(RawText)):
     HexOutput.append(hex(ord(RawText[i])))
     BinOutput.append(bin(ord(RawText[i])))
+    OctOutput.append(oct(ord(RawText[i])))
 HEX = " ".join(HexOutput)
 BIN = " ".join(BinOutput)
+OCT = " ".join(OctOutput)
 #Output
 print(f"\nHexadecimal : \n{HEX}")
 print(f"\nBinary : \n{BIN}")
+print(f"\nOctal : \n{OCT}")
