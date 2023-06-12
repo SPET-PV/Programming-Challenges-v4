63fb9d585322c20f2accb589eb1fc3ea650a074f
Spet-PV <SPET-PV@protonmail.com>
2023-06-12 01:30:44 +0100

    Text to Hex/Bin Betav0.1

--- Challenge66/prototype.py
+++ Challenge66/prototype.py
@@ -1,3 +1,15 @@
 import string
 
-
+#Inputs
+RawText = input("Enter your text : \n")
+HexOutput = []
+BinOutput = []
+#Treatment
+for i in range(0,len(RawText)):
+    HexOutput.append(hex(ord(RawText[i])))
+    BinOutput.append(bin(ord(RawText[i])))
+HEX = " ".join(HexOutput)
+BIN = " ".join(BinOutput)
+#Output
+print(f"\nHexadecimal : \n{HEX}")
+print(f"\nBinary : \n{BIN}")
